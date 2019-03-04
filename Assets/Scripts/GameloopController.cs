c1151e831ac353b47be333c2ba635785b6d374bf
marckvs <al341516@uji.es>
2019-03-04 15:54:24 +0100

    game loop

---
+++ Assets/Scripts/GameloopController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameloopController : MonoBehaviour {
+
+    public GameObject MainMenuScreen;
+    public GameObject LevelMenuScreen;
+    public GameObject InGameMenu;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void OnNewGameButtonPressed()
+    {
+        MainMenuScreen.SetActive(false);
+        LevelMenuScreen.SetActive(true);
+    }
+
+    public void OnSelectLevelButtonPressed(int buildIndex)
+    {
+        LevelMenuScreen.SetActive(false);
+        InGameMenu.SetActive(true);
+        SceneManager.LoadScene(buildIndex);
+    }
+
+    public void OnBackButtonPressed(int buildIndex)
+    {
+        InGameMenu.SetActive(false);
+        LevelMenuScreen.SetActive(true);
+        SceneManager.LoadScene(buildIndex);
+    }
+}
